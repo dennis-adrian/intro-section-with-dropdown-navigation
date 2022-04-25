11da3b68b0d880e5b1db2181f435ee2002e8dbe5
dennis adrian <dennisguzmanbo@gmail.com>
2022-04-24 23:15:04 -0400

    Add mobile menu bar

---
+++ index.js
@@ -0,0 +1,7 @@
+function showMenu() {
+  document.getElementById("mobile-sidebar").style.display = "block";
+}
+
+function hideMenu() {
+  document.getElementById("mobile-sidebar").style.display = "none";
+}
